7bb535db1ba92ed2be3b0fdfa7847ed958f72d29
George Soderholm <georgesoderholm@gmail.com>
2020-08-20 14:44:26 -0600

    Add start method

--- lib/play.rb
+++ lib/play.rb
@@ -1,5 +1,18 @@
 class Play
   attr_reader :player1, :player2
   def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
   end
-end 
+
+  def start
+    "#{player1} vs. #{player2}"
+  end
+
+  def take_a_turn
+  end
+
+  def end_game
+  end
+  
+end
