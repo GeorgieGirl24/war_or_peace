796b14a0cbb644d5607643c0dd69181e0726be26
George Soderholm <georgesoderholm@gmail.com>
2020-08-19 23:00:35 -0600

    Edit test method

--- test/war_or_peace_runner_test.rb
+++ test/war_or_peace_runner_test.rb
@@ -18,22 +18,29 @@ class WarOrPeaceTest < Minitest::Test
     war_or_peace = WarOrPeace.new
     war_or_peace.create_cards
 
-    assert_equal 52, war_or_peace.playing_cards.length
+    assert_equal 52, war_or_peace.deck.length
   end
 
   def test_there_are_four_suits_in_the_deck
     skip
     war_or_peace = WarOrPeace.new
     war_or_peace.create_cards
-    variable = war_or_peace.create_cards.group_by {|card| card.class}
+    ()
 
     assert_equal 4, war_or_peace.variable.length
   end
 
-  def test_each_suit_has_thirteen_cards
+  def test_deck_has_four_different_sets_of_ranks
     skip
     war_or_peace = WarOrPeace.new
     war_or_peace.create_cards
+
+    (2..14).each do |rank|
+      cards_in_rank = war_or_peace.deck.count do |card|
+        p card.rank == rank
+      end
+      assert_equal 4, war_or_peace.cards_in_rank
+    end
   end
 
   def test_it_can_split_the_deck_into_two_hands
@@ -48,6 +55,16 @@ class WarOrPeaceTest < Minitest::Test
 
   def test_two_players_have_their_own_unique_hands
     skip
+    war_or_peace = WarOrPeace.new
+    war_or_peace.create_cards
+    war_or_peace.make_two_hands
+    war_or_peace.
+    deck1 = Deck.new(war_or_peace.hand1)
+    deck2 = Deck.new(war_or_peace.hand2)
+    player1 = Player.new('Megan', deck1)
+    player2 = Player.new('Aurora', deck2)
+
+
   end
 
 end
