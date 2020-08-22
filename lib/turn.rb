2755f0d466e32f4a4d60985f38a98b4173e385de
George Soderholm <georgesoderholm@gmail.com>
2020-08-22 09:56:04 -0600

    Add a type of turn: loss

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,22 +1,31 @@
 require 'pry'
 class Turn
-  attr_accessor :player1, :player2, :spoils_of_war, :winner
+  attr_accessor :player1, :player2, :spoils_of_war, :winner, :total_cards_removed_from_pile_cards
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
     @spoils_of_war = []
     @winner = winner
+    # @amount_of_spoils = amount_of_spoils
+    @total_cards_removed_from_pile_cards = total_cards_removed_from_pile_cards
   end
 
   def type
     if (@player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)) &&
       (@player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2))
-      :mutally_assured_destruction
-
+      if @player1.deck.cards.length < 3 || @player2.deck.cards.length < 3
+        :loss
+      else
+        :mutally_assured_destruction
+      end
+    # elsif @player1.deck.cards.length < 3 || @player2.deck.cards.length < 3
+    #   :loss
     elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
-      :war
-    elsif @player1.deck.cards.length < 3 || @player2.deck.cards.length < 3
-      :loss
+      if @player1.deck.cards.length < 3 || @player2.deck.cards.length < 3
+        :loss
+      else
+        :war
+      end 
     else
       :basic
     end
@@ -42,19 +51,31 @@ class Turn
 
   def pile_cards
     if type == :mutally_assured_destruction
+      player1_count = 0
+      player2_count = 0
       3.times do
         @player1.deck.remove_card
         @player2.deck.remove_card
+        player1_count += 1
+        player2_count += 1
       end
+      @total_cards_removed_from_pile_cards = player1_count + player2_count
     elsif type == :war
+      player1_count = 0
+      player2_count = 0
       3.times do
         @spoils_of_war << @player1.deck.remove_card
         @spoils_of_war << @player2.deck.remove_card
-
+        player1_count += 1
+        player2_count += 1
+        @total_cards_removed_from_pile_cards = player1_count + player2_count
       end
     elsif type == :basic
+      player1_count = 1
+      player2_count = 1
       @spoils_of_war << @player1.deck.remove_card
       @spoils_of_war << @player2.deck.remove_card
+      @total_cards_removed_from_pile_cards = player1_count + player2_count
     end
 
   end
@@ -64,7 +85,6 @@ class Turn
       @spoils_of_war.each do |card|
         winner.deck.add_card(card)
       end
-      amount_of_spoils = @spoils_of_war.length
     end
   end
 end
