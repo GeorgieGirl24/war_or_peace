1a7097c09b8560df6b34609ca4f7f6745f194ccb
George Soderholm <georgesoderholm@gmail.com>
2020-08-22 17:46:50 -0600

    Edit methods to eliminate pry and comments

--- lib/game.rb
+++ lib/game.rb
@@ -5,52 +5,47 @@ require './lib/turn'
 
 
 class Game
-  attr_reader :player1, :player2, :turn, :turns
+  attr_reader :player1, :player2
 
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
-    @turn = Turn.new(@player1, @player2)
     @turns = []
   end
 
   def start
     count = 1
-    while count <= 1_000_000 #|| player1.has_lost? == true || player2.has_lost? == false
-      a_turn_type = turn.type
-      turn.winner?
+    while count <= 1_000_000
+      if @player1.deck.cards.length < 3 && (@player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0))
+        puts "*~*~*~* #{@player2.name} has won the game! *~*~*~*"
+        break
+      elsif @player2.deck.cards.length < 3 && (@player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0))
+        puts "*~*~*~* #{@player1.name} has won the game! *~*~*~*"
+        break
+      end
+
+      turn = Turn.new(@player1, @player2)
       turn.pile_cards
       turn.award_spoils(turn.winner)
-      if a_turn_type == :basic
-        puts "Turn #{count}: #{turn.winner.name} won #{turn.total_cards_removed_from_pile_cards} cards"
-        puts "#{@player1.name} has #{@player1.deck.cards.length}"
-        puts "#{@player2.name} has #{@player2.deck.cards.length}"
-      elsif a_turn_type == :war
-        puts "WAR - #{turn.winner.name} won #{turn.total_cards_removed_from_pile_cards} cards"
-        puts "#{@player1.name} has #{@player1.deck.cards.length}"
-        puts "#{@player2.name} has #{@player2.deck.cards.length}"
-      elsif a_turn_type == :mutally_assured_destruction
-        puts "*mutually assured destruction* #{turn.total_cards_removed_from_pile_cards} cards removed from play"
-        puts "#{@player1.name} has #{@player1.deck.cards.length}"
-        puts "#{@player2.name} has #{@player2.deck.cards.length}"
+
+      if turn.type == :basic
+        puts "Turn #{count}: #{turn.winner.name} won #{turn.total_cards_won} cards"
+      elsif turn.type == :war
+        puts "WAR - #{turn.winner.name} won #{turn.total_cards_won} cards"
+      elsif turn.type == :mutally_assured_destruction
+        puts "*mutually assured destruction* #{turn.total_cards_won} cards removed from play"
       end
-      turn.spoils_of_war.clear
-      @turns << a_turn_type
-      # binding.pry
+
+      if @player1.has_lost? == true
+        puts "*~*~*~* #{@player1.name} has won the game! *~*~*~*"
+        break
+      elsif @player2.has_lost? == true
+        puts "*~*~*~* #{@player2.name} has won the game! *~*~*~*"
+        break
+      end
       count += 1
-      if @player1.has_lost? == true || @player2.has_lost? == true || a_turn_type == :loss
-        if @player1.has_lost? == false
-          "*~*~*~* #{@player1} has won the game! *~*~*~*"
-          break
-        elsif @player2.has_lost? == false
-          "*~*~*~* #{@player2} has won the game! *~*~*~*"
-          break
-        end
-
-      end
-      "---- DRAW ----"
     end
-
+    "---- DRAW ----"
   end
 
 end
