6195b2031a7a72441597e5bb54146d1b284e26dc
George Soderholm <georgesoderholm@gmail.com>
2020-08-23 10:48:50 -0600

    Add a method to deal out cards. Add a function to generate cards inside class

--- lib/game.rb
+++ lib/game.rb
@@ -2,24 +2,53 @@ require './lib/card'
 require './lib/deck'
 require './lib/player'
 require './lib/turn'
-
+require './lib/card_generator'
 
 class Game
-  attr_reader :player1, :player2
-
-  def initialize(player1, player2)
+  attr_reader :player1, :player2, :turns, :full_deck,
+              :hand1, :hand2, :name1, :name2,
+              :card_file, :card_generator
+  def initialize(name1, name2)
+    @name1 = name1
+    @name2 = name2
+    @card_file = File.open("cards.txt")
+    @card_generator = CardGenerator.new(card_file)
     @player1 = player1
     @player2 = player2
     @turns = []
+    @full_deck = []
+    @hand1 = []
+    @hand2 = []
+  end
+
+  def deal
+    card_generator.cards
+    @full_deck = card_generator.playing_cards
+    @full_deck.shuffle!
+    @hand1 = full_deck.shift(26)
+    @hand2 = full_deck.shift(26)
+    @deck1 = Deck.new(hand1)
+    @deck2 = Deck.new(hand2)
+    @player1 =  Player.new(name1, @deck1)
+    @player2 =  Player.new(name2, @deck2)
   end
 
   def start
     count = 1
     while count <= 1_000_000
-      if @player1.deck.cards.length < 3 && (@player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0))
+
+      if @player1.deck.cards.length < 3 &&
+        (@player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0))
+        @player1.deck.cards.collect do |card|
+          @player2.deck.add_card(card)
+        end
         puts "*~*~*~* #{@player2.name} has won the game! *~*~*~*"
         break
-      elsif @player2.deck.cards.length < 3 && (@player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0))
+      elsif @player2.deck.cards.length < 3 &&
+        (@player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0))
+        @player1.deck.cards << @player2.deck.cards
+        @player2.deck.cards.clear
+        @player1.deck.cards.flatten!
         puts "*~*~*~* #{@player1.name} has won the game! *~*~*~*"
         break
       end
@@ -37,15 +66,18 @@ class Game
       end
 
       if @player1.has_lost? == true
+        @turns << turn
         puts "*~*~*~* #{@player1.name} has won the game! *~*~*~*"
         break
       elsif @player2.has_lost? == true
+        @turns << turn
         puts "*~*~*~* #{@player2.name} has won the game! *~*~*~*"
         break
       end
       count += 1
+      @turns << turn
     end
-    "---- DRAW ----"
+    puts "---- DRAW ----" if count > 1_000_000
   end
 
 end
