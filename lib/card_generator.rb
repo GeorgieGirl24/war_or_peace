494cf87975652eb7099fad89263b490fe6598c2c
George Soderholm <georgesoderholm@gmail.com>
2020-08-22 22:27:54 -0600

    Edit the initialize method to take an argument

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -3,13 +3,14 @@ Dir.chdir(File.dirname(__FILE__))
 
 
 class CardGenerator
-  attr_reader :playing_cards
-  def initialize
+  attr_reader :playing_cards, :card_file
+  def initialize(card_file)
+    @card_file = File.open(card_file,"r")
     @playing_cards = []
   end
 
   def create_cards
-    File.open("cards.txt","r").each do |line|
+    @card_file.each do |line|
       a_card = line.chop.split(', ')
       make_a_card = Card.new(a_card[1], a_card[0], a_card[2])
       @playing_cards << make_a_card
