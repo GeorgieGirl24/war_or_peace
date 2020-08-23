5992bf84fbd82cf060ef7c650b3fe25078742f08
George Soderholm <georgesoderholm@gmail.com>
2020-08-23 17:54:04 -0600

    Edited a method, removed the generate cards function, revert to a previous pattern

--- lib/game.rb
+++ lib/game.rb
@@ -1,42 +1,16 @@
-require './lib/card'
-require './lib/deck'
-require './lib/player'
 require './lib/turn'
-require './lib/card_generator'
 
 class Game
-  attr_reader :player1, :player2, :turns, :full_deck,
-              :hand1, :hand2, :name1, :name2,
-              :card_file, :card_generator
-  def initialize(name1, name2)
-    @name1 = name1
-    @name2 = name2
-    @card_file = File.open("cards.txt")
-    @card_generator = CardGenerator.new(card_file)
+  attr_accessor :player1, :player2, :turns
+  def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
     @turns = []
-    @full_deck = []
-    @hand1 = []
-    @hand2 = []
-  end
-
-  def deal
-    card_generator.cards
-    @full_deck = card_generator.playing_cards
-    @full_deck.shuffle!
-    @hand1 = full_deck.shift(26)
-    @hand2 = full_deck.shift(26)
-    @deck1 = Deck.new(hand1)
-    @deck2 = Deck.new(hand2)
-    @player1 =  Player.new(name1, @deck1)
-    @player2 =  Player.new(name2, @deck2)
   end
 
   def start
     count = 1
     while count <= 1_000_000
-
       if @player1.deck.cards.length < 3 &&
         (@player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0))
         @player1.deck.cards.collect do |card|
