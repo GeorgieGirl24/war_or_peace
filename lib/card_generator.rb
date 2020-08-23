bd58bc20d0e2784d79c6a87e3f9a4014ecef0308
George Soderholm <georgesoderholm@gmail.com>
2020-08-22 21:45:44 -0600

    Add two files to pull in the .txt file

---
+++ lib/card_generator.rb
@@ -0,0 +1,19 @@
+require './lib/card'
+Dir.chdir(File.dirname(__FILE__))
+
+
+class CardGenerator
+  attr_reader :playing_cards
+  def initialize
+    @playing_cards = []
+  end
+
+  def create_cards
+    File.open("cards.txt","r").each do |line|
+      a_card = line.chop.split(', ')
+      make_a_card = Card.new(a_card[1], a_card[0], a_card[2])
+      @playing_cards << make_a_card
+    end
+    # require 'pry';binding.pry
+  end
+end
